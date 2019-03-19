1e31ab7efb3c3c988d9dcf9a8a87b76202928f10
SamBritt <sam.britt2@yahoo.com>
2019-03-19 13:31:43 -0500

    continue working

---
+++ scripts/address.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('#addressList');
+//button
+const saveWish = document.createElement('button');
+saveWish.textContent = 'Save Wishlist';
+saveWish.id = 'saveWish';
+//fieldset
+const fieldset = document.createElement('fieldset');
+//label
+const label = document.createElement('label');
+label.htmlFor = 'place';
+//input
+const input = document.createElement('input');
+input.id = 'place';
+
+
+
+const createWishFields = (labelOne, labelTwo) => {
+    
+}
+
+saveWish.addEventListener('click', (e) => {
+
+})
+console.log(saveWish);
+
+
+
+
+
