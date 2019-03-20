2adace2771df683e469790ff118fe98ba444b6c8
SamBritt <sam.britt2@yahoo.com>
2019-03-20 09:07:34 -0500

    no new code. Just working on a function to create elements

--- scripts/address.js
+++ scripts/address.js
@@ -1,3 +1,16 @@
+let createElement = (element, ...rest) => {
+    let creation = document.createElement(element);
+    let params = ['id', 'textContent'];
+    let restParams = rest;
+
+    params.forEach(element => {
+        let result = restParams.find(item => {
+            element === item;
+        })
+        console.log(result)
+    })
+}
+
 const container = document.querySelector('#addressList');
 //button
 const saveWish = document.createElement('button');
@@ -6,22 +19,37 @@ saveWish.id = 'saveWish';
 //fieldset
 const fieldset = document.createElement('fieldset');
 //label
-const label = document.createElement('label');
-label.htmlFor = 'place';
+const label1 = document.createElement('label');
+label1.htmlFor = 'favorite';
+label1.textContent = 'Favorite Thing: ';
+
+const label2 = document.createElement('label');
+label2.htmlFor = 'place';
+label2.textContent = 'Where to buy it: ';
+
 //input
-const input = document.createElement('input');
-input.id = 'place';
+const input1 = document.createElement('input');
+input1.id = 'favorite';
+input1.placeholder = 'Enter your favorite item to purchase'
+const input2 = document.createElement('input');
+input2.id = 'place';
+input2.placeholder = 'Enter your favorite item to purchase'
 
 
 
 const createWishFields = (labelOne, labelTwo) => {
-    
+
+}
+const displayInfo = () => {
+
 }
 
 saveWish.addEventListener('click', (e) => {
 
 })
 console.log(saveWish);
+
+createElement('li', 'id')
 
 
 
